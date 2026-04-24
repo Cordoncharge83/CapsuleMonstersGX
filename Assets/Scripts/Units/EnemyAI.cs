50e6f6dcdd4f91771c2dc9151a0eb6862528b270
Cordoncharge83 <146195205+Cordoncharge83@users.noreply.github.com>
2026-04-24 23:19:54 +0200

    feat: update enemy movement range

--- Assets/Scripts/Units/EnemyAI.cs
+++ Assets/Scripts/Units/EnemyAI.cs
@@ -29,27 +29,42 @@ public class EnemyAI : MonoBehaviour
 
     private void MoveTowardPlayer()
     {
-        Vector3Int enemyCell = enemyUnit.GetCurrentCellPosition();
-        Vector3Int playerCell = playerUnit.GetCurrentCellPosition();
-
-        Vector3Int direction = Vector3Int.zero;
-
-        int deltaX = playerCell.x - enemyCell.x;
-        int deltaY = playerCell.y - enemyCell.y;
-
-        if (Mathf.Abs(deltaX) > Mathf.Abs(deltaY))
+        for (int i = 0; i < enemyUnit.GetMoveRange(); i++)
         {
-            direction.x = deltaX > 0 ? 1 : -1;
-        }
-        else if (deltaY != 0)
-        {
-            direction.y = deltaY > 0 ? 1 : -1;
-        }
+            if (enemyUnit.IsInAttackRange(playerUnit))
+            {
+                return;
+            }
 
-        Vector3Int targetCell = enemyCell + direction;
+            Vector3Int enemyCell = enemyUnit.GetCurrentCellPosition();
+            Vector3Int playerCell = playerUnit.GetCurrentCellPosition();
 
-        if (combatTilemap.HasTile(targetCell))
-        {
+            Vector3Int direction = Vector3Int.zero;
+
+            int deltaX = playerCell.x - enemyCell.x;
+            int deltaY = playerCell.y - enemyCell.y;
+
+            if (Mathf.Abs(deltaX) > Mathf.Abs(deltaY))
+            {
+                direction.x = deltaX > 0 ? 1 : -1;
+            }
+            else if (deltaY != 0)
+            {
+                direction.y = deltaY > 0 ? 1 : -1;
+            }
+
+            Vector3Int targetCell = enemyCell + direction;
+
+            if (!combatTilemap.HasTile(targetCell))
+            {
+                return;
+            }
+
+            if (targetCell == playerUnit.GetCurrentCellPosition())
+            {
+                return;
+            }
+
             enemyUnit.MoveTo(targetCell);
         }
     }
