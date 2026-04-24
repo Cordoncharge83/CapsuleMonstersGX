241786e45417689fbfbaf6dfb6c61a59370e0bfb
Cordoncharge83 <146195205+Cordoncharge83@users.noreply.github.com>
2026-04-24 15:55:00 +0200

    feat: spawn player unit on grid

---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Unit : MonoBehaviour
+{
+    [SerializeField] private Tilemap combatTilemap;
+    [SerializeField] private Vector3Int currentCellPosition;
+
+    private void Start()
+    {
+        SnapToCell(currentCellPosition);
+    }
+
+    public void SnapToCell(Vector3Int cellPosition)
+    {
+        currentCellPosition = cellPosition;
+
+        Vector3 worldPosition = combatTilemap.GetCellCenterWorld(cellPosition);
+        transform.position = worldPosition;
+    }
+}
