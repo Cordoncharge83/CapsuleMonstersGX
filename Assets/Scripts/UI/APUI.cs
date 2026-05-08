a7be58af23fde2d355ea446194152a1ddd57fd94
Cordoncharge83 <146195205+Cordoncharge83@users.noreply.github.com>
2026-05-08 20:55:05 +0200

    feat: AP numbers decrease gradually

--- Assets/Scripts/UI/APUI.cs
+++ Assets/Scripts/UI/APUI.cs
@@ -1,7 +1,7 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 
 public class APUI : MonoBehaviour
 {
@@ -12,36 +12,77 @@ public class APUI : MonoBehaviour
     [Header("Bar")]
     [SerializeField] private Image fillImage;
 
+    [Header("Animation")]
+    [SerializeField] private float fillDuration = 0.35f;
+    [SerializeField] private float numberStepDelay = 0.02f;
+
+    private Coroutine fillCoroutine;
+    private Coroutine numberCoroutine;
+
+    private int displayedAP = -1;
+
     public void UpdateAP(int currentAP, int maxAP)
     {
-        // Text
-        currentAPText.text = currentAP.ToString();
         gainText.text = $"(+{maxAP})";
 
-        // Bar
-        if (maxAP > 0)
+        AnimateNumber(currentAP);
+        AnimateBar(currentAP, maxAP);
+    }
+
+    private void AnimateNumber(int targetAP)
+    {
+        if (numberCoroutine != null)
         {
-            StartCoroutine(AnimateFill((float)currentAP / maxAP));
+            StopCoroutine(numberCoroutine);
         }
-        else
+
+        numberCoroutine = StartCoroutine(AnimateNumberRoutine(targetAP));
+    }
+
+    private IEnumerator AnimateNumberRoutine(int targetAP)
+    {
+        if (displayedAP < 0)
         {
-            fillImage.fillAmount = 0f;
+            displayedAP = targetAP;
+            currentAPText.text = displayedAP.ToString();
+            yield break;
         }
+
+        while (displayedAP != targetAP)
+        {
+            displayedAP += displayedAP < targetAP ? 1 : -1;
+            currentAPText.text = displayedAP.ToString();
+
+            yield return new WaitForSeconds(numberStepDelay);
+        }
+
+        numberCoroutine = null;
+    }
+
+    private void AnimateBar(int currentAP, int maxAP)
+    {
+        if (fillCoroutine != null)
+        {
+            StopCoroutine(fillCoroutine);
+        }
+
+        float targetFill = maxAP > 0 ? (float)currentAP / maxAP : 0f;
+        fillCoroutine = StartCoroutine(AnimateFill(targetFill));
     }
 
     private IEnumerator AnimateFill(float target)
     {
         float start = fillImage.fillAmount;
         float time = 0f;
-        float duration = 0.35f;
 
-        while (time < duration)
+        while (time < fillDuration)
         {
             time += Time.deltaTime;
-            fillImage.fillAmount = Mathf.Lerp(start, target, time / duration);
+            fillImage.fillAmount = Mathf.Lerp(start, target, time / fillDuration);
             yield return null;
         }
 
         fillImage.fillAmount = target;
+        fillCoroutine = null;
     }
 }
